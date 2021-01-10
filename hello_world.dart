ced132440660672e28c70bd93aa0ea93b6047b0b
ldn <leduynhac@gmail.com>
2021-01-10 10:31:14 +0700

    update README.md + rename file

---
+++ hello_world.dart
@@ -0,0 +1,10 @@
+//define a function
+void sayHi(String name){
+  print("Hi $name");
+}
+
+//define the entry point where the app starts executing
+void main() {
+  String name = "John Doe";
+  sayHi(name);
+}
